3f120b331213df1cb4d0fce6b3d85ecf1e5f3397
Dakota Kallas <kallas6990@uwlax.edu>
2022-12-15 08:43:06 -0600

    Started working on projects page

---
+++ js/project.js
@@ -0,0 +1,18 @@
+let projects = [
+  {
+    "project-image": "images/mini-c-compiler.png",
+    "project-image-alt": "compiler-img",
+    "project-name": "Mini C Compiler",
+    "project-brief": "Simple compiler for a C-like language.",
+  },
+  {
+    "project-image": "images/personal-webpage.png",
+    "project-image-alt": "webpage-img",
+    "project-name": "First Personal Webpage",
+    "project-brief": "Personal website made with HTML/CSS.",
+  },
+];
+
+function addProjects() {
+  let projectContainer = document.getElementById("projects");
+}
