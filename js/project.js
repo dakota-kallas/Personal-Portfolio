bad150496c1e21298b40180ab061012429f373fc
Dakota Kallas <kallas6990@uwlax.edu>
2022-12-15 22:46:52 -0600

    Added more project functionality

--- js/project.js
+++ js/project.js
@@ -1,25 +1,47 @@
 let projects = [
   {
+    "project-id": "goodfight",
     "project-image": "images/goodfight.png",
     "project-image-alt": "goodfight-img",
     "project-name": "The Goodfight",
     "project-brief": "JavaFX application for Non-profit.",
+    "project-url": "https://github.com/dakota-kallas/GoodFight-JavaFX-App",
   },
   {
+    "project-id": "webpage",
     "project-image": "images/personal-webpage.png",
     "project-image-alt": "webpage-img",
     "project-name": "First Personal Webpage",
     "project-brief": "Personal website made with HTML/CSS.",
+    "project-url": "https://github.com/dakota-kallas/Personal-Webpage",
   },
   {
+    "project-id": "bills-burgers",
     "project-image": "images/bills-burgers.png",
     "project-image-alt": "bills-burgers-img",
     "project-name": "Restaurant Website",
     "project-brief": "HTML/CSS site made to practice.",
+    "project-url": "https://github.com/dakota-kallas/Restaurant-Site",
+  },
+  {
+    "project-id": "compiler",
+    "project-image": "images/compiler.png",
+    "project-image-alt": "compiler-img",
+    "project-name": "Mini-C Compiler",
+    "project-brief": "Compiler written for a C-like language.",
+    "project-url": "https://github.com/dakota-kallas/Mini-C-Compiler",
+  },
+  {
+    "project-id": "weather-collection",
+    "project-image": "images/weather-collection.png",
+    "project-image-alt": "weather-collection-img",
+    "project-name": "Weather Collection",
+    "project-brief": "A simple site demonstrating AJAX.",
+    "project-url": "https://github.com/dakota-kallas/Weather-Collection",
   },
 ];
 
-document.addEventListener("DOMContentLoaded", loadContent);
+window.onload = loadContent;
 
 function loadContent() {
   addProjects();
@@ -31,11 +53,16 @@ function addProjects() {
   projects.forEach((project) => {
     var projectCard = document.createElement("div");
     projectCard.className = "project-card";
+    projectCard.id = project["project-id"];
+    projectCard.URL = project["project-url"];
 
+    var projectImgContainer = document.createElement("div");
+    projectImgContainer.className = "project-img-container";
     var projectImg = document.createElement("img");
     projectImg.setAttribute("src", project["project-image"]);
     projectImg.setAttribute("alt", project["project-image-alt"]);
-    projectCard.appendChild(projectImg);
+    projectImgContainer.appendChild(projectImg);
+    projectCard.appendChild(projectImgContainer);
 
     var projectDescriptionContainer = document.createElement("div");
     projectDescriptionContainer.className = "project-description-container";
@@ -53,5 +80,40 @@ function addProjects() {
     projectCard.appendChild(projectDescriptionContainer);
 
     projectContainer.appendChild(projectCard);
+    projectCard.addEventListener("mouseenter", addProjectHover);
+    projectCard.addEventListener("mouseleave", removeProjectHover);
   });
+}
+
+function addProjectHover(e) {
+  console.log("entered");
+  var projectCard = e.currentTarget;
+  var projectImgContainer = projectCard.firstChild;
+  var projectImg = projectImgContainer.firstChild;
+  projectImg.classList += " blurred";
+
+  var viewButtonContent = document.createElement("a");
+  viewButtonContent.setAttribute("href", projectCard.URL);
+  viewButtonContent.setAttribute("target", "_blank");
+  var viewButtonContainer = document.createElement("div");
+  viewButtonContainer.className = "view-link";
+  var viewButton = document.createElement("p");
+  var viewButtonText = document.createTextNode("View");
+  viewButtonContainer.appendChild(viewButton);
+  viewButton.appendChild(viewButtonText);
+  var viewButtonOpen = document.createElement("img");
+  viewButtonOpen.setAttribute("src", "images/open.png");
+  viewButtonOpen.setAttribute("alt", "open-img");
+  viewButtonContainer.appendChild(viewButtonOpen);
+  viewButtonContent.appendChild(viewButtonContainer);
+  projectImgContainer.appendChild(viewButtonContent);
+}
+
+function removeProjectHover(e) {
+  console.log("left");
+  var projectCard = e.currentTarget;
+  var projectImgContainer = projectCard.firstChild;
+  var projectImg = projectImgContainer.firstChild;
+  projectImg.classList.remove("blurred");
+  projectImgContainer.removeChild(projectImgContainer.children[1]);
 }
